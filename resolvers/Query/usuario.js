1f6ac478ffc8eb2bddbf22648772a979daa82eb3
Alexandre Igari <djun_igari@hotmail.com>
2021-10-02 10:49:51 +0900

    first commit

---
+++ resolvers/Query/usuario.js
@@ -0,0 +1,43 @@
+const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const { getUsuarioLogado } = require('../comum/usuario')
+
+module.exports = {
+    async login(_,{dados}){
+        const usuario = await db('usuarios')
+            .where({email: dados.email}).first()
+        
+        if(!usuario){
+            throw new Error('Usuario/Senha invalido')
+        }
+
+        const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha)
+        
+        if(!saoIguais){
+            throw new Error('Usuario/Senha invalido')
+        }
+
+        return getUsuarioLogado(usuario)
+    },
+    usuarios(parent, args, ctx) {
+        ctx && ctx.validarAdmin()
+        return db('usuarios')
+    },
+    usuario(_, { filtro }, ctx) {
+        ctx && ctx.validarUsuarioFiltro(filtro)
+        
+        if(!filtro) return null
+        const { id, email } = filtro
+        if(id) {
+            return db('usuarios')
+                .where({ id })
+                .first()
+        } else if(email) {
+            return db('usuarios')
+                .where({ email })
+                .first()
+        } else {
+            return null
+        }
+    },
+}
